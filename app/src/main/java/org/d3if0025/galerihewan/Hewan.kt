1f9b716e4cff2807cc1f8bb57dd86df6ddc2f847
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-05-03 22:54:52 +0700

    3.5.Menampilkan gambar dari internet

--- app/src/main/java/org/d3if0025/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if0025/galerihewan/Hewan.kt
@@ -3,6 +3,6 @@ package org.d3if0025.galerihewan
 data class Hewan (
     val nama : String,
     val namaLatin : String,
-    val imageResId: Int = R.drawable.angsa,
+    val imageId: String,
     val deskripsi : String
 )
--- app/src/main/java/org/d3if0025/galerihewan/MainAdapter.kt
+++ app/src/main/java/org/d3if0025/galerihewan/MainAdapter.kt
@@ -3,7 +3,9 @@ package org.d3if0025.galerihewan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.d3if0025.galerihewan.databinding.ListItemBinding
+import org.d3if0025.galerihewan.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     private val data = mutableListOf<Hewan>()
@@ -12,29 +14,32 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         data.addAll(newData)
         notifyDataSetChanged()
     }
+
     class ViewHolder(private val binding: ListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            descTextView.text = hewan.deskripsi
-            imageView.setImageResource(hewan.imageResId)
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                descTextView.text = hewan.deskripsi
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
-    }
-
-
-    override fun getItemCount(): Int {
-        return data.size
+        TODO("Not yet implemented")
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position])
+        TODO("Not yet implemented")
     }
 
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
